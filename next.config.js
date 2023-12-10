eff7c18b7d460a8a186061aaf52c08ddf187d286
Sag@0418 <sagarathani0418@gmail.com>
2023-12-10 15:10:20 +0530

    updated the hero page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com','avatars.githubusercontent.com','icons8.com','gamma.app'], // Add the required domains here
+      },
+      
+}
 
 module.exports = nextConfig
