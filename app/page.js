eead2911ef900303f535193e51e438f82b5dd1bc
Sag@0418 <sagarathani0418@gmail.com>
2023-12-10 16:48:13 +0530

    added the header

--- app/page.js
+++ app/page.js
@@ -1,25 +1,11 @@
 "use client";
 import "../styles/global.css"
+import Navbar from '../component/Navbar';
 
 const Home = () => (
-    <section className='w-full flex-center flex-col mt-20'>
-      <h1 className="head_text text-center my-5">
-        <div className='text-transparent bg-gradient-to-r from-black to-black bg-clip-text'>Explore the World of Coding</div>
-        <br className="hidden md:inline-block" />
-        <span>&lt;</span>
-        <span>/&gt;</span>
-      </h1>
-      <p className="text-3xl text-green-600 text-center mt-10 bg-gradient-to-r from-green-400 to-green-600 text-transparent bg-clip-text my-15">
-      "Code on letsCode and 
-      </p>
-      <p className="text-3xl text-green-600 text-center mt-5 bg-gradient-to-r from-green-400 to-green-600 text-transparent bg-clip-text my-15">
-      inspire the world with your passion for programming."</p>
-      <div className='mt-5'></div>
-      <div className='flex justify-center animate-bounce mt-5'>
-        <a href='/problems' className='btn-grad'>
-          letsCode</a>
-      </div>
-    </section>
+  <>
+    <Navbar/>
+  </>
 );
 
 export default Home;
---
+++ component/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Link from 'next/dist/client/link';
+import Image from 'next/image';
+import "../styles/global.css";
+
+const Navbar = () => {
+  const [isNavbarVisible, setNavbarVisibility] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setNavbarVisibility(!isNavbarVisible);
+  };
+
+  return (
+    <header className="header">
+
+      <Link href="/" className="logo">
+        ReadMe
+      </Link>
+
+      <nav className={`navbar ${isNavbarVisible ? 'show' : ''}`}>
+        <Link href="#home">Home</Link>
+        <Link href="#services">Services</Link>
+        <Link href="#about">About Us</Link>
+        <Link href="#contact">Contact Us</Link>
+      </nav>
+
+      <div className="hamburger" onClick={handleHamburgerClick}>
+      <Image
+            src="/grid-outline.svg" 
+            alt="logo"
+            width={180}
+            height={90}
+      />
+      </div>
+
+    </header>
+  );
+};
+
+export default Navbar;
