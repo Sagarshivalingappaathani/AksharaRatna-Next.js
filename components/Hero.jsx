9d94d1d0c21b9f78b116697c1519ad71621b4a09
Sag@0418 <sagarathani0418@gmail.com>
2024-01-02 23:59:38 +0530

    added the files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,10 +7,8 @@ function Hero() {
       <section className="h-screen bg-cover" style={{ backgroundImage: `url('./hero1.png')` }}>
         <div className="flex h-full w-full items-center justify-center container mx-auto px-8">
           <div className="max-w-5xl text-center">
-            <h1 className="text-3xl font-bold mb-2 sm:text-5xl capitalize tracking-widest green lg:text-7xl animate-fade-in">ಒಮ್ಮೆ ಒಬ್ಬ ಬುದ್ಧಿವಂತನು ಹೇಳಿದನು</h1>
-            <p id="typing-text" className="mt-6 lg:text-lg font-bold text-white"></p>
             <div className="mt-8 flex flex-col space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0">
-              <h2> "ಪುಸ್ತಕವು ನೀವು ಮತ್ತೆ ಮತ್ತೆ ತೆರೆಯಬಹುದಾದ ಉಡುಗೊರೆಯಾಗಿದೆ."</h2>
+              <h2 className='text-black font-bold'> "ನಾನು ಸ್ವರ್ಗ ಹೇಗಿರುತ್ತದೆ ಎಂದು ಊಹಿಸಿಕೊಂಡಾಗ, ಅದೊಂದು ಪುಸ್ತಕಾಲಯದಂತೆ ಕಾಣಿಸುತ್ತದೆ' ಎಂಬ ಜಾರ್ಜ್‌ ಲೂಯಿಸ್‌ ಬೋರ್ಜಸ್‌ರ ಮಾತಿನಲ್ಲಿ ಅತಿಶಯವೇನಿಲ್ಲ! ಪುಸ್ತಕಗಳು ನೀಡುವ ಸುಖ, ಓದುಗನಿಗೆ ಮಾತ್ರ ಅರ್ಥವಾಗಲು ಸಾಧ್ಯ"</h2>
             </div>
           </div>
         </div>
