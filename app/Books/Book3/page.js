28e855ded717875ecdfffd1efd525bf739f35f40
Sag@0418 <sagarathani0418@gmail.com>
2024-01-03 22:42:56 +0530

    added the files

--- app/Books/Book3/page.js
+++ app/Books/Book3/page.js
@@ -30,64 +30,6 @@ const Page= () => {
           </p>
         </div>
       </div>
-      <section className=" text-gray-100">
-        <div className="container px-6 py-12 mx-auto">
-          <div className="grid items-center gap-4 xl:grid-cols-5">
-            <div className="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left">
-              <h2 className="text-4xl font-bold text-orange-600">What Our Reader Says</h2>
-              <p className="text-orange-600">Pri ex magna scaevola moderatius. Nullam accommodare no vix, est ei diceret alienum, et sit cetero malorum. Et sea iudico consequat, est sanctus adipisci ex.</p>
-            </div>
-            <div className="p-6 xl:col-span-3">
-              <div className="grid gap-4 md:grid-cols-2">
-                <div className="grid content-center gap-4">
-                  <div className="p-6 rounded shadow-md bg-gray-900">
-                    <p>An audire commodo habemus cum. Ne sed corrumpit repudiandae.An audire commodo habemus cum. Ne sed corrumpit repudiandae.</p>
-                    <div className="flex items-center mt-4 space-x-4">
-                      <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
-                      <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
-                        <p className="text-sm text-gray-400">CTO of Company Co.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="p-6 rounded shadow-md bg-gray-900">
-                    <p>Sit wisi sapientem ut, pri civibus temporibus voluptatibus et, ius cu hinc fabulas. </p>
-                    <div className="flex items-center mt-4 space-x-4">
-                      <img src="https://source.unsplash.com/50x50/?portrait?2" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
-                      <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
-                        <p className="text-sm text-gray-400">CTO of Company Co.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="grid content-center gap-4">
-                  <div className="p-6 rounded shadow-md bg-gray-900">
-                    <p>Putant omnium elaboraret per ut. Id dicta tritani nominavi quo, mea id justo errem elaboraret.</p>
-                    <div className="flex items-center mt-4 space-x-4">
-                      <img src="https://source.unsplash.com/50x50/?portrait?3" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
-                      <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
-                        <p className="text-sm text-gray-400">CTO of Company Co.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="p-6 rounded shadow-md bg-gray-900">
-                    <p>Te omnes virtute volutpat sed. Ei esse eros interesset vel, Te omnes virtute volutpat sed. Ei esse eros interesset vel,</p>
-                    <div className="flex items-center mt-4 space-x-4">
-                      <img src="https://source.unsplash.com/50x50/?portrait?4" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
-                      <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
-                        <p className="text-sm text-gray-400">CTO of Company Co.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
     </>
   );
 };
